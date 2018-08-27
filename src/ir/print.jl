2e882af27fa3a5faf51719c2969d90a7bc06328d
Mike J Innes <mike.j.innes@gmail.com>
2018-08-27 12:39:11 +0100

    simplify printing

--- src/ir/print.jl
+++ src/ir/print.jl
@@ -1,17 +1,14 @@
 import Base: show
 
-function show(io::IO, ir::IR)
-  defs = Dict((b, i) => s for (s, (b, i)) in enumerate(ir.defs))
-  for b in 1:length(ir.blocks)
-    println(io, b, ":")
-    for i in 1:length(ir.blocks[b].stmts)
-      print(io, lpad(string("%", defs[(b, i)]), 4), " = ")
-      st = ir.blocks[b].stmts[i]
-      st.expr == nothing ? println(io, "nothing") :
-        println(io, st.expr)
-    end
-    for g in ir.blocks[b].gotos
-      println(io, "       ", g.expr)
-    end
+function show(io::IO, b::Block)
+  println(io, b.id, ":")
+  for (x, st) in b
+    x == nothing ?
+      print(io, "       ") :
+      print(io, lpad(string("%", x.id), 4), " = ")
+    st.expr == nothing ? println(io, "nothing") :
+      println(io, st.expr)
   end
 end
+
+show(io::IO, ir::IR) = foreach(b -> show(io, b), blocks(ir))
