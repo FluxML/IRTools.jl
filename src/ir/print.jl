0322ffcd27c414de380dd7a127572a3f881db505
Mike Innes <mike.j.innes@gmail.com>
2018-08-27 15:58:07 +0100

    update printing

--- src/ir/print.jl
+++ src/ir/print.jl
@@ -3,9 +3,8 @@ import Base: show
 function show(io::IO, b::Block)
   println(io, b.id, ":")
   for (x, st) in b
-    x == nothing ?
-      print(io, "       ") :
-      print(io, lpad(string("%", x.id), 4), " = ")
+    print(io, "  ")
+    x == nothing || print(io, string("%", x.id), " = ")
     st.expr == nothing ? println(io, "nothing") :
       println(io, st.expr)
   end
