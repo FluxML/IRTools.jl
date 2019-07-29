ee5078157950b04d977d8a60b011aaaf6452d46c
Mike J Innes <mike.j.innes@gmail.com>
2019-07-29 16:25:23 +0100

    test dynamo refresh

--- test/compiler.jl
+++ test/compiler.jl
@@ -18,6 +18,11 @@ add(a, b) = a+b
 @test roundtrip(add, 2, 3) == 5
 @test passthrough(add, 2, 3) == 5
 
+IRTools.refresh(roundtrip)
+
+add(a, b) = a*b
+@test roundtrip(add, 2, 3) == 6
+
 relu(x) = x > 0 ? x : 0
 @test roundtrip(relu, 1) == 1
 @test passthrough(relu, 1) == 1
