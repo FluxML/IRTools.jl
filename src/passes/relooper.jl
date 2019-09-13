3da7da800142beb1aa58c371a1a91a7abb87ae81
Mike J Innes <mike.j.innes@gmail.com>
2019-09-13 11:58:09 +0100

    fixes

--- src/passes/relooper.jl
+++ src/passes/relooper.jl
@@ -52,7 +52,8 @@ function reaches_unique(rs)
   Dict(b => filter(c -> !any(rs -> c in rs, others(b)), cs) for (b, cs) in rs)
 end
 
-function reloop_loop(cfg::CFG, blocks, entry, done, rs)
+function reloop_loop(cfg::CFG, blocks, entry, done)
+  rs = Dict(b => reaching(cfg, b, ignore = done) for b in blocks)
   body = filter(b -> any(e -> e in rs[b], entry), blocks)
   next = setdiff(blocks, body)
   Loop(reloop(cfg, blocks = body, entry = entry, done = union(done, entry)),
@@ -60,15 +61,15 @@ function reloop_loop(cfg::CFG, blocks, entry, done, rs)
  end
 
 function reloop(cfg::CFG; blocks = 1:length(cfg.graph), entry = [1], done = Int[])
-  isempty(blocks) && return
-  rs = Dict(b => reaching(cfg, b, ignore = done) for b in blocks)
+  (isempty(blocks) || isempty(entry)) && return
+  rs = Dict(b => reaching(cfg, b, ignore = done) for b in entry)
   if length(entry) == 1 && entry[] ∉ rs[entry[]]
     next = setdiff(blocks, entry[])
     Simple(entry[],
            reloop(cfg, blocks = next,
                   entry = intersect(cfg.graph[entry[]], blocks), done = done))
   elseif all(b -> isempty(setdiff(entry, rs[b])), entry)
-    reloop_loop(cfg, blocks, entry, done, rs)
+    reloop_loop(cfg, blocks, entry, done)
   elseif (rsu = reaches_unique(rs); !all(isempty, values(rsu)))
     unique = filter(b -> !isempty(rsu[b]), entry)
     inner = [reloop(cfg, blocks = union(b, rsu[b]), entry = [b], done = done) for b in unique]
@@ -76,19 +77,32 @@ function reloop(cfg::CFG; blocks = 1:length(cfg.graph), entry = [1], done = Int[
     es = intersect(next, union(entry, reaching.((cfg,), unique)...))
     Multiple(inner, reloop(cfg, blocks = next, entry = es, done = done))
   else
-    reloop_loop(cfg, blocks, entry, done, rs)
+    reloop_loop(cfg, blocks, entry, done)
   end
 end
 
+const indent = "    "
+
 printstructure(io::IO, ::Nothing, level) = nothing
 
+_printstructure(io::IO, s, level) = printstructure(io, s, level)
+
+function _printstructure(io::IO, s::Simple, level)
+  println(io, indent^level, s.block)
+  _printstructure(io, s.next, level)
+end
+
 function printstructure(io::IO, s::Simple, level)
-  println(io, "  "^level, s.block)
-  printstructure(io, s.next, level)
+  if s.next == nothing
+    _printstructure(io, s, level)
+  else
+    println(io, indent^level, "Simple:")
+    _printstructure(io, s, level+1)
+  end
 end
 
 function printstructure(io::IO, s::Multiple, level)
-  println(io, "  "^level, "Multiple:")
+  println(io, indent^level, "Multiple:")
   for b in s.inner
     printstructure(io, b, level+1)
   end
@@ -96,12 +110,12 @@ function printstructure(io::IO, s::Multiple, level)
 end
 
 function printstructure(io::IO, s::Loop, level)
-  println(io, "  "^level, "Loop:")
+  println(io, indent^level, "Loop:")
   printstructure(io, s.inner, level+1)
   printstructure(io, s.next, level)
 end
 
 function Base.show(io::IO, b::Union{Simple,Multiple,Loop})
   println(io, "Structured CFG:")
-  printstructure(io, b, 0)
+  _printstructure(io, b, 0)
 end
