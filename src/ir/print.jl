4e457023ffae618cab04cb9e1ca362f8543379fc
Mike Innes <mike.j.innes@gmail.com>
2019-09-24 17:22:43 +0100

    customisable printing

--- src/ir/print.jl
+++ src/ir/print.jl
@@ -3,6 +3,8 @@ import Base: show
 # TODO: real expression printing
 Base.show(io::IO, x::Variable) = print(io, "%", x.id)
 
+print_stmt(io::IO, ex) = print(io, ex)
+
 function show(io::IO, b::Branch)
   if b == unreachable
     print(io, "unreachable")
@@ -19,29 +21,39 @@ function show(io::IO, b::Branch)
   end
 end
 
+const tab = "  "
+
+function printargs(io::IO, args, types = [Any for arg in args])
+  print(io, "(")
+  for i = 1:length(args)
+    print(io, args[i])
+    types[i] != Any && print(io, " :: ", types[i])
+    i != length(args) && print(io, ", ")
+  end
+  print(io, ")")
+end
+
+# TODO avoid trailing newline
 function show(io::IO, b::Block)
+  indent = get(io, :indent, 0)
   bb = basicblock(b)
+  print(io, tab^indent)
   print(io, b.id, ":")
   if !isempty(bb.args)
-    print(io, " (")
-    for i = 1:length(bb.args)
-      print(io, bb.args[i])
-      bb.argtypes[i] != Any && print(io, " :: ", bb.argtypes[i])
-      i != length(bb.args) && print(io, ", ")
-    end
-    print(io, ")")
+    print(io, " ")
+    printargs(io, bb.args, bb.argtypes)
   end
   println(io)
   for (x, st) in b
-    print(io, "  ")
+    print(io, tab^indent, "  ")
     x == nothing || print(io, string("%", x.id), " = ")
     st.expr == nothing ? print(io, "nothing") :
-      print(io, st.expr)
+      print_stmt(io, st.expr)
     st.type == Any || print(io, " :: ", st.type)
     println(io)
   end
   for br in bb.branches
-    println(io, "  ", br)
+    println(io, tab^indent, "  ", br)
   end
 end
 
