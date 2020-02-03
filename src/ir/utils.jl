eb15dfa22d7d54bb12006d74e19569303ab355ae
Philipp Gabler <phipsgabler@users.noreply.github.com>
2020-02-03 10:37:32 +0100

    Make xcall take kwargs; add XCall module wrapper

--- src/ir/utils.jl
+++ src/ir/utils.jl
@@ -9,10 +9,6 @@ walk(x::PhiNode, inner, outer) =
 
 walk(x::PiNode, inner, outer) = outer(PiNode(inner(x.val), x.typ))
 
-xcall(mod::Module, f::Symbol, args...) = Expr(:call, GlobalRef(mod, f), args...)
-xcall(f::Symbol, args...) = xcall(Base, f, args...)
-xcall(f, args...) = Expr(:call, f, args...)
-
 map(f, br::Branch) = Branch(br, condition = f(br.condition), args = f.(br.args))
 
 function map(f, b::BasicBlock)
@@ -76,3 +72,55 @@ function exprline(ir::IR, x::Variable)
   i > 0 || return
   get(ir.lines, ir[x].line, nothing)
 end
+
+
+@eval begin
+    # since var"#module#" does not work in pre-1.3
+    struct XCall
+        $(Symbol("#module#"))::Module
+    end
+end
+
+function getproperty(x::XCall, name::Symbol)
+    mod = getfield(x, Symbol("#module#"))
+    if name === Symbol("#module#")
+        return mod
+    else
+        return (args...; kwargs...) -> xcall(mod, name, args...; kwargs...)
+    end
+end
+
+
+"""
+`BaseCall.<bla>(args...; kwargs...)` is a hack to produce the properly namespaced expression for
+BaseCall.<bla>(args...; kwargs...).
+"""
+const BaseCall = XCall(Base)
+
+
+function xcall(_f, args...; kwargs...)
+    if isempty(kwargs)
+        Expr(:call, _f, args...)
+    else
+        keys = QuoteNode[]
+        values = Any[]
+        for (k, v) in kwargs
+            push!(keys, QuoteNode(k))
+            push!(values, v)
+        end
+
+        _call(args...) = Expr(:call, args...)
+        _apply_type = GlobalRef(Core, :apply_type)
+        _NamedTuple = GlobalRef(Core, :NamedTuple)
+        _tuple = GlobalRef(Core, :tuple)
+        _kwfunc = GlobalRef(Core, :kwfunc)
+        
+        _namedtuple = _call(_apply_type, _NamedTuple, _call(_tuple, keys...))
+        _kws = _call(_namedtuple, _call(_tuple, values...))
+        return _call(_call(_kwfunc, _f), _kws, _f, args...)
+    end
+end
+
+xcall(mod::Module, f::Symbol, args...; kwargs...) =
+    xcall(GlobalRef(mod, f), args...; kwargs...)
+xcall(f::Symbol, args...; kwargs...) = xcall(GlobalRef(Base, f), args...; kwargs...)
