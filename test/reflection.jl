9c1426a8d3dda5f9a8c10b8fc3c9835e899782ef
MasonProtter <mason.protter@icloud.com>
2020-06-14 11:20:36 -0600

    support do blocks in @code_ir

--- test/reflection.jl
+++ test/reflection.jl
@@ -6,3 +6,7 @@ using IRTools: Meta, meta
 @test meta(Tuple{typeof(gcd),Int,Int}) isa Meta
 
 @test meta(Tuple{typeof(f),Int}) isa Meta
+
+@test @code_ir(map([1,2], [3,4]) do x, y
+  x + y
+end) isa IR
